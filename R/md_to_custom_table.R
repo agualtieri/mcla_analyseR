8a9e9cdf61a337190af85c5d8b49f9e5693c7b5f
agualtieri <alberto.gualtieri@reach-initiative.org>
2019-10-03 15:57:24 +0300

    Latest Update

--- R/md_to_custom_table.R
+++ R/md_to_custom_table.R
@@ -1,9 +1,16 @@
-my_custom_md_table<- function (result) 
-{
+my_custom_md_table <- function (result) {
+  
   md_out <- ""
+  
+  names <- list(name = c("a1_1", "a1_2", "a1_3", "a1_4", "a1_5"),
+                label = c("Non displaced", "IDPS", "Returnees", "Refugees", "Migrants"))
+  
+  
   if (!is.null(result$summary.statistic)) {
     if (!is.null(result$parameters$independent.var)) {
-      hypegrammaR:::md_add_lines(md_out) <- (paste("by", result$results$summary.statistic$repeat.var.value))
+    
+      hypegrammaR:::md_add_lines(md_out) <- (paste("by", names$label[match(result$summary.statistic$repeat.var.value[1], names$name)]))
+    
     }
     table <- result %>% (hypegrammaR:::map_to_table)
     hypegrammaR:::md_add_lines(md_out) <- knitr::kable(table, format = "html") %>% 
@@ -11,10 +18,14 @@ my_custom_md_table<- function (result)
   }
   if (!is.null(result$hypothesis.test$result$p.value)) {
     hypegrammaR:::md_add_lines(md_out) <- c(as.character(unique(result$hypothesis.test$name)), 
-                                            "P Value:")
+                              "P Value:")
     result$hypothesis.test$result$p.value %>% print
   }
   hypegrammaR:::md_add_lines(md_out) <- knitr::kable(result$hypothesis.test %>% 
-                                                       as.data.frame, format = "html")
+                                         as.data.frame, format = "html")
   md_out
 }
+
+
+
+
--- mcla_analysis_v1.R
+++ mcla_analysis_v1.R
@@ -1,7 +1,7 @@
 ## MCLA 2019 Analysis
 rm(list = ls())
 
-# Install packages
+# Install packages (please uncomment these lines the first time you run the code)
 # devtools::install_github("mabafaba/hypegrammaR", force = T, build_vignettes = T)
 # devtools::install_github("mabafaba/xlsformfill", force = T, build_vignettes = T)
 # devtools::install_github("mabafaba/composr", force = T, build_vignettes = T)
@@ -13,8 +13,10 @@ library("composr")
 library("tidyverse")
 library("kableExtra")
 
-#browseVignettes("hypegrammaR")
 
+# Source
+source("./R/cleanHead.R")
+source("./R/md_to_custom_table.R")
 
 
 # load questionnaire inputs
@@ -27,7 +29,7 @@ questions$relevant<-tolower(questions$relevant)
 questions$calculation<-tolower(questions$calculation)
 questions$type<-tolower(questions$type)
 
-choices <- read.csv("input/choices_named.csv", 
+choices <- read.csv("input/choices_named_v2.csv", 
                     stringsAsFactors=F, check.names=F)
 
 # remove empty columns
@@ -46,10 +48,10 @@ choices <- choices[, colnames(choices)!=""]
 
 ### Create fake dataset for testing
 # sampling frame
-samplingframe <- load_samplingframe("./input/sf_governorate.csv")
+samplingframe <- load_samplingframe("./input/sf_district.csv")
 
 # make samplingframe tidy (one row per stratum)
-samplingframe_tidy <- samplingframe %>% gather(key = "population_group",
+samplingframe <- samplingframe %>% gather(key = "population_group",
                                                value = "population",
                                                idps,
                                                migrants,
@@ -57,37 +59,42 @@ samplingframe_tidy <- samplingframe %>% gather(key = "population_group",
                                                refugees,
                                                returnees) %>%
   # select interesting columns only 
-  select(governorate_code, governorate_name, population_group, population )
+  select(district_code, district_name, population_group, population )
 
 
 # remove non existent combinations
-samplingframe_tidy<- samplingframe_tidy %>% filter(!is.na(population))
+samplingframe <- samplingframe %>% filter(!is.na(population))
 # add stratum id to sampling frame
-samplingframe_tidy<- samplingframe_tidy %>% mutate(stratum_id = paste0(governorate_code,"_",population_group))
+samplingframe <- samplingframe %>% mutate(stratum_id = paste0(district_code,"_",population_group))
 # remove zeros
-samplingframe_tidy<- filter(samplingframe_tidy, population >0)
+samplingframe <- filter(samplingframe, population >0)
 
 
-# add district pcodes to choice list
-#sf_based_choices <- samplingframe_tidy %>% 
- # mutate(list_name = "governorate") %>%
-#  rename(name = governorate_code,`label::English` = governorate_name) %>%
- # select(list_name, name,`label::English`)
+# Add district pcodes to choice list
+  #sf_based_choices <- samplingframe_tidy %>% 
+  #mutate(list_name = "governorate") %>%
+  #rename(name = governorate_code,`label::English` = governorate_name) %>%
+  #select(list_name, name,`label::English`)
 
 
-#choices <- plyr::rbind.fill(choices, sf_based_choices)
-#choices <- choices[!duplicated(choices$name),]
+# choices <- plyr::rbind.fill(choices, sf_based_choices)
+# choices <- choices[!duplicated(choices$name),]
 
-# generate data
-#questions[questions$name %>% grepl("a3_me",.) %>% which,] %>% t
-#choices[choices$list_name=="district","name"]
-
-
-response <- xlsform_fill(questions,choices, 50)
+# Generate dummy data for testing
+# questions[questions$name %>% grepl("a3_me",.) %>% which,] %>% t
+# choices[choices$list_name=="district","name"]
+# response <- xlsform_fill(questions,choices, 27)
 
 
+# Load Pre-Test sample data
+response <- read.csv("input/MCLA_pre test_response.csv", stringsAsFactors = F)
 names(response)<- koboquest:::to_alphanumeric_lowercase(names(response))
 
+### Delete unnecesary columns
+response <- response %>% select(-c("start", "end", "deviceid", "x_tags", "x_notes", "x__version__", "meta.instanceid", "meta.instancename"))
+
+# Truncate column headers so that they can be read by the script
+response <- cleanHead(response)
 
 # horizontal operations / recoding
 # ... (none for now)
@@ -95,7 +102,7 @@ names(response)<- koboquest:::to_alphanumeric_lowercase(names(response))
 # vertical operations / aggregation
 
 ### Load the questionnare
-questionnaire <- load_questionnaire(response,questions,choices,choices.label.column.to.use = "label::English")
+questionnaire <- load_questionnaire(response,questions,choices,choices.label.column.to.use = "label::REACH")
 
 
 ### create stratm_id into the response dataset
@@ -109,14 +116,11 @@ response <- response %>%
   recode_to("migrants",where.selected.any = "a1_5") %>% 
   end_recoding()
 
-
-
-response <- mutate(response, stratum_id = paste0(a2_metadata,"_",data_stratum_id))
-
+response <- mutate(response, stratum_id = paste0(a3_metadata,"_",data_stratum_id))
 
 
 ### Weighting function
-weight.function <- map_to_weighting(sampling.frame = samplingframe_tidy, 
+weight.function <- map_to_weighting(sampling.frame = samplingframe, 
                                     data.stratum.column = "stratum_id",
                                     sampling.frame.population.column = "population",
                                     sampling.frame.stratum.column = "stratum_id")
@@ -125,13 +129,13 @@ weight.function <- map_to_weighting(sampling.frame = samplingframe_tidy,
 
 
 ### Delete non-matching obs (it shouldn't happen with a real dataset)
-response <- response[(response$stratum_id %in% samplingframe_tidy$stratum_id),]
+response <- response[(response$stratum_id %in% samplingframe$stratum_id),]
 
 weight.function(response) %>% write.csv("./output/weighted_dataset.csv", row.names = F) 
 browseURL("./output/weighted_dataset.csv")
 
-### Delete unnecesary columns
-response <- response %>% select(-c("start", "end", "deviceid", "formname"))
+# Truncate column headers so that they can be read by the script
+response <- cleanHead(response)
 
 ### Create an analysis plan, define the case, and run the analysis function
 #source("functions/analysisplan_factory.R")
@@ -142,46 +146,47 @@ response <- response %>% select(-c("start", "end", "deviceid", "formname"))
 
 #write.csv(analysis_plan, "./output/test_analysisplan.csv")
 
-analysis_plan <- read.csv("./input/test_analysis_plan_v2.csv", 
+
+analysis_plan <- read.csv("./input/test_analysis_plan_v3.csv", 
                           stringsAsFactors=F, check.names=F)
 
 
 response_analysis_output <- from_analysisplan_map_to_output(data = response,
                                                             weighting = weight.function,
                                                             analysisplan = analysis_plan,
-                                                            questionnaire = hypegrammaR::load_questionnaire(response, questions, choices, choices.label.column.to.use = "label::English"),
+                                                            questionnaire = hypegrammaR::load_questionnaire(response, questions, choices, choices.label.column.to.use = "label::REACH"),
                                                             labeled = T,
                                                             verbose = T)
 
 
+
 ### Create bulk two-way tables from analysis output
-# response_analysis_output[["analysisplan"]][["sub.research.question"]]
-
-
-
 twowaytables <- response_analysis_output$results %>% lapply(map_to_table)
 
-filenames <- paste0("MCLA_analysis_output_", response_analysis_output[["analysisplan"]][["sub.research.question"]], ".xlsx") 
+names <- list(name = c("a1_1", "a1_2", "a1_3", "a1_4", "a1_5"),
+             label = c("nondisplaced", "idps", "returnees", "refugees", "migrants")) %>% as_tibble
+                  
 
-response_tables <- purrr::map2(twowaytables, filenames, xlsx::write.xlsx)
+filenames <- paste0("MCLA_analysis_output_", response_analysis_output$analysisplan$sub.research.question,"_",
+                    names$label[match(response_analysis_output$analysisplan$repeat.var.value, names$name)], ".xlsx") 
 
-
-
+purrr::map2(twowaytables, filenames, xlsx::write.xlsx)
+  
 
 
 ### Easier to read .html file for analysis presentation
 response_analysis_output$analysisplan
 hypegrammaR:::map_to_generic_hierarchical_html(resultlist = response_analysis_output,
-                                               render_result_with = my_custom_md_table, 
+                                               render_result_with = my_custom_md_table,
                                                by_analysisplan_columns = c("research.question","sub.research.question"),
                                                by_prefix = c("RQ:", "Indicator: "),
                                                level = 2,
-                                               questionnaire = load_questionnaire(response, questions, choices, choices.label.column.to.use = "label::English"),
+                                               questionnaire = load_questionnaire(response, questions, choices, choices.label.column.to.use = "label::REACH"),
                                                dir = "./output",
                                                filename = "test_output.html")
 
 # hypegrammaR:::from_result_map_to_md_table, 
-my_custom_md_table
+# my_custom_md_table
 
 ### Map to master table for pivoting and other cross indicator analysis
 response_analysis_output$results[[1]]$summary.statistic
